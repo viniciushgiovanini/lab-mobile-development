c1631727fa6c1bbab034ee8f517d04d13a538988
Vinícius Henrique Giovanini <vhgiovanini@gmail.com>
2022-09-20 10:00:21 -0300

    TP01_input e output funcionando

--- Trabalhos/01TP_Divide_Conta/dividecontaapp/lib/main.dart
+++ Trabalhos/01TP_Divide_Conta/dividecontaapp/lib/main.dart
@@ -4,14 +4,52 @@ import 'package:flutter/material.dart';
 //Widgets Statefull --> COM alteração de Estado
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const divideC());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class divideC extends StatefulWidget {
+  const divideC({Key? key}) : super(key: key);
 
   @override
+  State<divideC> createState() => _divideConta();
+}
+
+class _divideConta extends State<divideC> {
+  //Variaveis da classe
+  var valorTotal = TextEditingController();
+  var qtdPessoas = TextEditingController();
+  double contaFinal = 0.0;
+
   Widget build(BuildContext context) {
-    return Row(children: [Text("OIE")]);
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text("Dividir Conta")),
+      body: BodyBody(),
+    ));
+  }
+
+  BodyBody() {
+    return Column(children: [
+      TextFormField(
+          controller: valorTotal,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(labelText: "Valor Total")),
+      TextFormField(
+          controller: qtdPessoas,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(labelText: "Quantidade de Pessoas")),
+      ElevatedButton(onPressed: calculoConta, child: Text("Calcular")),
+      Text('O valor a ser divido é: ${contaFinal.toString()}')
+    ]);
+  }
+
+  calculoConta() {
+    double quantidadeP = double.parse(qtdPessoas.text),
+        quantidadeValor = double.parse(valorTotal.text);
+    double valorFinal = 0.0;
+    valorFinal = quantidadeValor / quantidadeP;
+    setState(() {
+      contaFinal = valorFinal;
+    });
   }
 }
